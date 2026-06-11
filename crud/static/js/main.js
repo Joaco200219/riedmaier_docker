c72b93a19e6b092b8ddf839e0f52ce833f2c0aa3
Joaco200219 <salamecondulcedeleche@gmail.com>
2026-06-11 14:32:00 -0300

    finiquita

--- crud/static/js/main.js
+++ crud/static/js/main.js
@@ -22,11 +22,11 @@ document.addEventListener('DOMContentLoaded', () => {
         dark: 'https://bootswatch.com/5/darkly/bootstrap.min.css'
     };
 
-    // 1. Cargar la preferencia guardada al iniciar la página (por defecto es 'light')
+
     const savedTheme = localStorage.getItem('user-theme') || 'light';
     themeStyle.setAttribute('href', themes[savedTheme]);
 
-    // 2. Escuchar los clics en las opciones del menú desplegable
+
     dropdownItems.forEach(item => {
         item.addEventListener('click', (e) => {
             e.preventDefault(); // Evita que la página salte al hacer clic
